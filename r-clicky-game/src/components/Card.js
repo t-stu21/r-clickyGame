e9d870e6f5f84078d7fe04454563239349633e90
tyler stuart <t.stu21.11@gmail.com>
2019-06-28 16:24:06 -0700

    basic setup

---
+++ r-clicky-game/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+  <div className="card" onClick={props.imageClick}>
+    <div className="img-container">
+      <img
+        alt={props.image.replace(".jpg", "")}
+        src={require("../../images/" + props.image)}
+      />
+    </div>
+  </div>
+);
+
+export default Card;
